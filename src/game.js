f1400144c198bb6ec009d2c4a8d4c90b79c28fea
jon <jondell518@gmail.com>
2014-01-25 01:23:24 -0500

    Added in some sprite grids

--- src/game.js
+++ src/game.js
@@ -19,11 +19,25 @@ define(
 				var stage = new P.Stage(),
 				    renderer = new P.CanvasRenderer(config.width, config.height),
 
-				    sprite = new P.Sprite(P.Texture.fromImage("tempBoxRed.png"));
+				    //sprite = new P.Sprite(P.Texture.fromImage("tempBoxRed.png")),
+				    row,column;
+
+				    for(row =0;row<config.world.length; row++)
+				    {
+				    	for(column =0; column<config.world[0].length; column++)
+				    	{
+				    		sprite = new P.Sprite(P.Texture.fromImage("tempBoxRed.png"));
+				    		sprite.position.x = column*(config.tileWidth+2);
+				    		sprite.position.y = row*(config.tileHeight+2);
+				    		stage.addChild(sprite);
+
+
+				    	}
+				    }
 				
 				document.body.appendChild(renderer.view)
 
-				stage.addChild(sprite);
+				
 				renderer.render(stage);
 				
 				
