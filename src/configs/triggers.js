780a6a4ead04268a32d51021d3c2a810faf5c020
jon <jondell518@gmail.com>
2014-01-26 06:40:49 -0500

    triggers reborn

---
+++ src/configs/triggers.js
@@ -0,0 +1,39 @@
+define(
+	[
+		"config",
+		"../minigames/puzzleGame",
+		"../dialog"
+	],
+	function (config, Puzzle, Dialog) {
+		config.triggers = {
+			"10,17": function (game, next) {
+				if (game.progress > 0)
+					next();
+				else {
+					(new Puzzle(game.renderer, game.drunk, function(result){
+
+						if(result){
+							game.drunk++;
+							game.renderer.view.className = "drunk" + game.drunk;
+						}
+						console.log("BEFORE X: " + game.bunny.position.x/config.tileWidth);
+						console.log("BEFORE Y: " + game.bunny.position.y/config.tileHeight);
+						game.bunny.position.y = 11*config.tileHeight;
+						game.bunny.position.x = 16*config.tileWidth;
+
+						console.log("AFTER X: " + game.bunny.position.x/config.tileWidth);
+						console.log("AFTER Y: " + game.bunny.position.y/config.tileHeight);
+
+
+
+
+						next();
+					}
+
+						
+					)).start();
+				}
+			}
+		};
+	}
+);
